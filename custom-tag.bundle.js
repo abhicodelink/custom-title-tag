26c744da7d3e48a55ee75db6d1b7b47ab887ca0d
Abhi <103061615+abhicodelink@users.noreply.github.com>
2022-09-01 15:39:46 +0530

    Add files via upload

---
+++ custom-tag.bundle.js
@@ -0,0 +1,34 @@
+
+class big_title extends HTMLElement {
+  constructor() {
+    super();
+
+
+
+  }
+}
+
+window.customElements.define("big-title", big_title);
+
+class mid_title extends HTMLElement {
+  constructor() {
+    super();
+
+  }
+}
+
+window.customElements.define("mid-title", mid_title);
+
+class sm_title extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+
+window.customElements.define("sm-title", sm_title);
+
+class donate extends HTMLElement {
+  constructor() {
+    
+  }
+}
